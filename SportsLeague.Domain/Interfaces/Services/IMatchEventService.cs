974827b0827b23e6f4908a36756703c08d7bd1b3
Julian <juliancithop69@gmail.com>
2026-05-22 18:46:39 -0500

    Phase 5

---
+++ SportsLeague.Domain/Interfaces/Services/IMatchEventService.cs
@@ -0,0 +1,29 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface IMatchEventService
+    {
+        #region Match Results
+        Task<MatchResult> RegisterResultAsync(int matchId, MatchResult result);
+        Task<MatchResult?> GetResultByMatchAsync(int matchId);
+        #endregion
+
+        #region  Goals
+        Task<Goal> RegisterGoalAsync(int matchId, Goal goal);
+        Task<IEnumerable<Goal>> GetGoalsByMatchAsync(int matchId);
+        Task DeleteGoalAsync(int goalId);
+        #endregion
+
+        #region  Cards
+        Task<Card> RegisterCardAsync(int matchId, Card card);
+        Task<IEnumerable<Card>> GetCardsByMatchAsync(int matchId);
+        Task DeleteCardAsync(int cardId);
+        #endregion
+
+    }
+
+}
