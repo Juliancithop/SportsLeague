db0cc6e4adc34f857fb0b1118db82f726683880c
Julian <juliancithop69@gmail.com>
2026-05-22 21:52:26 -0500

    Phase 6 completed

---
+++ SportsLeague.Domain/Interfaces/Services/IStandingsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Services
+{
+    public interface IStandingsService
+    {
+        Task<object> GetStandingsAsync(int tournamentId); //Obtener la tabla de posiciones apra un torneo especifico
+        Task<object> GetTopScorersAsync(int tournamentId); //Obtener la lista de los maximos goleadores para un torneo en especifico
+
+        Task<object> GetCardStatsAsync(int tournamentId);// Obtener las estadistivcas de tarjetas para un torneo en especifico
+    }
+
+}
